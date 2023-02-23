e2afa57e5c1b9d2b9335f3bd4ae7fea7bfbfc0f1
Junior <jscinnamon7483@gmail.com>
2023-02-23 13:48:57 +0100

    script d'import fonctionnel: ajout de la gestion d'erreur ( + url invalid)

--- import.py
+++ import.py
@@ -24,28 +24,37 @@ def get_quizz_filename(categorie, titre, difficulte):
 def generate_json_file(categorie, titre, url):
     out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
     out_questions_data = []
-    response = requests.get(url)
-    data = json.loads(response.text)
-    all_quizz = data["quizz"]["fr"]
-    for quizz_title, quizz_data in all_quizz.items():
-        out_filename = get_quizz_filename(categorie, titre, quizz_title)
-        print(out_filename)
-        out_questionnaire_data["difficulte"] = quizz_title
-        for question in quizz_data:
-            question_dict = {}
-            question_dict["titre"] = question["question"]
-            question_dict["choix"] = []
-            for ch in question["propositions"]:
-                question_dict["choix"].append((ch, ch==question["réponse"]))
-            out_questions_data.append(question_dict)
-        out_questionnaire_data["questions"] = out_questions_data
-        out_json = json.dumps(out_questionnaire_data)
+    try:
+        response = requests.get(url)
+    except:
+        print("Exception  pour la requete: " + url)
+    else:      
+        try:
+            data = json.loads(response.text)
+            all_quizz = data["quizz"]["fr"]
+            for quizz_title, quizz_data in all_quizz.items():
+                out_filename = get_quizz_filename(categorie, titre, quizz_title)
+                print(out_filename)
+                out_questionnaire_data["difficulte"] = quizz_title
+            for question in quizz_data:
+                question_dict = {}
+                question_dict["titre"] = question["question"]
+                question_dict["choix"] = []
+                for ch in question["propositions"]:
+                    question_dict["choix"].append((ch, ch==question["réponse"]))
+                out_questions_data.append(question_dict)
+            out_questionnaire_data["questions"] = out_questions_data
+            out_json = json.dumps(out_questionnaire_data)
 
-        file = open(out_filename, "w")
-        file.write(out_json)
-        file.close()
-        print("end")
+            file = open(out_filename, "w")
+            file.write(out_json)
+            file.close()
+            print("end")
 
+        except:
+            print("Exception data pour l'url : " + url + " -question: " + titre)
+        
+    
 
 for quizz_data in open_quizz_db_data:
     generate_json_file(quizz_data[0], quizz_data[1], quizz_data[2])
